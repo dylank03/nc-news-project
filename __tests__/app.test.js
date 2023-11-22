0e4b38c4e8e205452adfb62ab9c1fc43f3b7dd87
dylank03 <dylankataria@outlook.com>
2023-11-22 10:10:08 +0000

    resolved merge conflicts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -64,7 +64,6 @@ describe('endpoint GET /api/articles/:article_id',()=>{
         .get('/api/articles/2')
         .expect(200)
         .then(({body})=>{
-            console.log(body)
             expect(body.article).toEqual({article_id: 2, 
             title: "Sony Vaio; or, The Laptop", 
             topic: "mitch",
